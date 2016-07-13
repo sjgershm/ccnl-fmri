3666b9d4717403e25a6234bded0dc055b52a8e2f
samgershman <sam.gershman@gmail.com>
2016-07-13 16:44:12 -0400

    updated con function

--- ccnl_fmri_con.m
+++ ccnl_fmri_con.m
@@ -40,38 +40,42 @@ function ccnl_fmri_con(EXPT,model,contrasts,subjects)
     
     cd(cdir);
     
+    %% save contrasts
+    modeldir0 = fullfile(EXPT.modeldir,['model',num2str(model)]);
+    save(fullfile(modeldir0,'contrasts'),'contrasts','convec');
+    
     %% Group-level analysis
-    modeldir = fullfile(EXPT.modeldir,['model',num2str(model)]);
-    save(fullfile(modeldir,'contrasts'),'contrasts','convec');
-    if exist(fullfile(modeldir,'SPM.mat'),'file'); delete(fullfile(modeldir,'SPM.mat')); end
-    cd(modeldir)
-    job.dir{1} = modeldir;
-    job.multi_cov = []; job.cov = []; job.globalc.g_omit = 1;
-    job.globalm.glonorm = 1; job.globalm.gmsca.gmsca_no = 1;
-    job.masking.tm.tm_none = 1; job.masking.em{1} = [];
     for j = 1:length(contrasts)
+        modeldir = fullfile(EXPT.modeldir,['model',num2str(model)],['con',num2str(j)]);
+        if isdir(modeldir); rmdir(modeldir,'s'); end
+        mkdir(modeldir);
+        cd(modeldir)
+        job.dir{1} = modeldir;
+        job.multi_cov = []; job.cov = []; job.globalc.g_omit = 1;
+        job.globalm.glonorm = 1; job.globalm.gmsca.gmsca_no = 1;
+        job.masking.tm.tm_none = 1; job.masking.em{1} = [];
+        
         con = sprintf('con_%04d.nii',j);
         for s = 1:length(subjects)
-            job.des.t1.scans{s} = fullfile(modeldir,['subj',num2str(subjects(s))],con);
+            job.des.t1.scans{s} = fullfile(modeldir0,['subj',num2str(subjects(s))],con);
         end
-    end
-    out = spm_run_factorial_design(job);
-    load(out.spmmat{1});
-    SPM = spm_spm(SPM);
-    
-    % write contrasts and t-maps
-    matlabbatch = [];
-    matlabbatch{1}.spm.stats.con.spmmat{1} = fullfile(modeldir,'SPM.mat');
-    matlabbatch{1}.spm.stats.con.delete = 1;
-    for j = 1:length(contrasts)
+        
+        out = spm_run_factorial_design(job);
+        load(out.spmmat{1});
+        SPM = spm_spm(SPM);
+        
+        % write contrasts and t-maps
+        matlabbatch = [];
+        matlabbatch{1}.spm.stats.con.spmmat{1} = fullfile(modeldir,'SPM.mat');
+        matlabbatch{1}.spm.stats.con.delete = 1;
         convec = zeros(size(SPM.xX.name));
         ix = strcmp(SPM.xX.name,'mean');
         convec(ix) = 1;
         matlabbatch{1}.spm.stats.con.consess{j}.tcon.name = contrasts{j};
         matlabbatch{1}.spm.stats.con.consess{j}.tcon.convec = convec;
         matlabbatch{1}.spm.stats.con.consess{j}.tcon.sessrep = 'none';
+        
+        spm_jobman('run',matlabbatch);
+        
+        cd(cdir);
     end
-    
-    spm_jobman('run',matlabbatch);
-    
-    cd(cdir);
