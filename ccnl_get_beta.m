300115ca289845d5420a88872ea14261813548b6
Momchil Tomov <tomov90@gmail.com>
2018-08-07 20:00:31 -0400

    refactor ccnl_get_beta and ccnl_get_tmaps

--- ccnl_get_beta.m
+++ ccnl_get_beta.m
@@ -20,27 +20,9 @@ function beta = ccnl_get_beta(EXPT,model,regressor,mask,subjects)
     % Sam Gershman, Nov 2016
     
     if nargin < 4; subjects = 1:length(EXPT.subject); end
-   
-    % figure out how voxels are provided
-    type = 'none';
-    if ischar(mask)
-        % load mask
-        Vmask = spm_vol(mask);
-        Ymask = spm_read_vols(Vmask);
-        mask = Ymask~=0 & ~isnan(Ymask);
-        type = 'mask';
-    else
-        if ndims(mask) == 3
-            type = 'mask';
-        elseif ndims(mask) == 2
-            if size(mask,2) == 3
-                type = 'mni';
-            elseif size(mask,1) == 1 || size(mask,2) == 1
-                type = 'inds_or_binary';
-            end
-        end
-    end
-    disp(type);
+
+    % load mask
+    [mask_format, mask, Vmask] = get_mask_format_helper(mask);
 
     if ischar(regressor)
         regressor = strtrim(regressor);
@@ -51,41 +33,8 @@ function beta = ccnl_get_beta(EXPT,model,regressor,mask,subjects)
         modeldir = fullfile(EXPT.modeldir,['model',num2str(model)],['subj',num2str(subj)]);
         load(fullfile(modeldir,'SPM.mat'));
        
-        n = 0; clear y
-        for i = 1:length(SPM.xX.name)
-            if ischar(regressor) && (~isempty(strfind(SPM.xX.name{i},[regressor,'*'])) || ~isempty(strfind(SPM.xX.name{i},[regressor,'^']))) ...
-               || ~ischar(regressor) && i == regressor
-                n = n + 1;
-                V = spm_vol(fullfile(modeldir,sprintf('beta_%04d.nii',i)));    % residual variance image
-                Y = spm_read_vols(V);
-               
-                switch type
-                    
-                    case 'inds_or_binary' % a list of voxel indices or a binary vector in NATIVE space
-                        y(n,:) = Y(mask);
-
-                    case 'mni' % a list of MNI (!) coordinates
-                        cor = mni2cor(mask, V.mat);
-                        inds = sub2ind(size(Y),cor(:,1),cor(:,2),cor(:,3));
-                        y(n,:) = Y(inds);
-
-                    case 'mask' % a 3D mask in MNI or native space
-                        if all(size(Y) == size(mask))
-                            y(n,:) = Y(mask);
-                        else
-                            [mask_cor1, mask_cor2, mask_cor3] = ind2sub(size(mask),find(mask==1));
-                            cor = mni2cor(cor2mni([mask_cor1 mask_cor2 mask_cor3], Vmask.mat),V.mat);
-                            inds = sub2ind(size(Y),cor(:,1),cor(:,2),cor(:,3));
-                            y(n,:) = Y(inds);
-                        end
-
-                    otherwise
-                        assert(false, 'Error: incorrect mask or voxel list');
-                end
-                
-            end
-        end
+        b = get_beta_or_tmap_helper(regressor, modeldir, Vmask, mask, SPM.xX.name, mask_format, 'beta');
         
-        beta(s,:) = nanmean(y, 1);
+        beta(s,:) = nanmean(b, 1);
         
     end
--- ccnl_get_tmap.m
+++ ccnl_get_tmap.m
@@ -1,6 +1,7 @@
 function tmap = ccnl_get_tmap(EXPT,model,regressor,mask,subjects)
     
     % Extract t-statistics from a mask. Also see ccnl_get_beta.
+    % Works only for single-regressor contrasts.
     %
     % USAGE: tmap = ccnl_get_tmap(EXPT,model,regressor,mask,[subjects])
     %
@@ -18,13 +19,9 @@ function tmap = ccnl_get_tmap(EXPT,model,regressor,mask,subjects)
     % Momchil Tomov, Jul 2017
     
     if nargin < 4; subjects = 1:length(EXPT.subject); end
-    
+   
     % load mask
-    if ischar(mask)
-        Vmask = spm_vol(mask);
-        Ymask = spm_read_vols(Vmask);
-        mask = Ymask~=0 & ~isnan(Ymask);
-    end
+    [mask_format, mask, Vmask] = get_mask_format_helper(mask);
 
     if ischar(regressor)
         regressor = strtrim(regressor);
@@ -37,11 +34,11 @@ function tmap = ccnl_get_tmap(EXPT,model,regressor,mask,subjects)
         subj = subjects(s);
         modeldir = fullfile(EXPT.modeldir,['model',num2str(model)],['subj',num2str(subj)]);
 
-        t = get_beta_or_tmap_helper(regressor, modeldir, Vmask, mask, contrasts, 'spmT');
+        t = get_beta_or_tmap_helper(regressor, modeldir, Vmask, mask, contrasts, mask_format, 'spmT');
     
         if size(t, 1) > 1
             load(fullfile(modeldir,'SPM.mat'));
-            b = get_beta_or_tmap_helper(regressor, modeldir, Vmask, mask, SPM.xX.name, 'beta');
+            b = get_beta_or_tmap_helper(regressor, modeldir, Vmask, mask, SPM.xX.name, mask_format, 'beta');
             assert(isequal(size(b), size(t)), 'Should have 1 t-map only or exactly 1 t-map per regressor');
 
             se = b ./ t;
@@ -54,28 +51,3 @@ function tmap = ccnl_get_tmap(EXPT,model,regressor,mask,subjects)
     end
 
 end
-
-
-% helper function
-%
-function y = get_beta_or_tmap_helper(regressor, modeldir, Vmask, mask, names, prefix)
-    n = 0;
-    for i = 1:length(names)
-        if ischar(regressor) && (~isempty(strfind(names{i},[regressor,'*'])) || ~isempty(strfind(names{i},[regressor,'^'])) || endsWith(names{i},regressor)) ...
-           || ~ischar(regressor) && i == regressor
-            n = n + 1;
-            V = spm_vol(fullfile(modeldir,sprintf('%s_%04d.nii',prefix,i)));    % residual variance or t-statistic image
-            Y = spm_read_vols(V);
-           
-            if all(size(Y) == size(mask))
-                y(n,:) = Y(mask);
-            else
-                [mask_cor1, mask_cor2, mask_cor3] = ind2sub(size(mask),find(mask==1));
-                cor = mni2cor(cor2mni([mask_cor1 mask_cor2 mask_cor3], Vmask.mat),V.mat);
-                inds = sub2ind(size(Y),cor(:,1),cor(:,2),cor(:,3));
-                y(n,:) = Y(inds);
-            end
-        end
-    end
-end
-
