1e0130ad1093fb59eb82d5b2109e6c5b556f05f1
Momchil Tomov <tomov90@gmail.com>
2019-01-29 14:03:29 -0500

    typo

--- ccnl_load_mask.m
+++ ccnl_load_mask.m
@@ -6,7 +6,7 @@ function [mask, V, Y] = ccnl_load_mask(mask_filename)
     %   mask_filename = path to .nii file with mask
     %
     % OUTPUT:
-    %   mask = 3D binary mask for the non-0 non-nan voxels from Ymask
+    %   mask = 3D binary mask for the non-0 non-nan voxels from Y
     %   V = volume struct from spm_vol, could be used to save the mask with spm_write_vol(V, Y) or spm_write_vol(V, mask). IMPORTANT: make sure to change V.fname first! Otherwise, will overwrite original file
     %   Y = mask loaded with from spm_read_vols 
     %
