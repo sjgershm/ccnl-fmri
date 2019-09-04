cde433a99852d223691393ce8351c5b6e6e069d6
Wouter Kool <wouter.kool@gmail.com>
2019-09-04 16:24:45 -0500

    Add files via upload

---
+++ ccnl_fmri_glm_ants.m
@@ -0,0 +1,75 @@
+function ccnl_fmri_glm_ants(EXPT,model,subjects,fake)
+    
+    % Estimate subject-level GLM on non-normalized functionals. Note: this
+    % function should only be used after preprocessing data with
+    % ccnl_fmri_preproc_ants.m.
+    % 
+    % If fake = true, only creates the SPM.mat files, without actually 
+    % running the GLM. Use for quick sanity checks.
+    %
+    % USAGE: ccnl_fmri_glm_ants(EXPT,model,[subjects])
+    
+    cdir = pwd;
+    if nargin < 3; subjects = 1:length(EXPT.subject); end
+    if nargin < 4; fake = false; end
+
+    % create models folder if none exists
+    if ~isdir(EXPT.modeldir); mkdir(EXPT.modeldir); end
+    if ~isdir(fullfile(EXPT.modeldir,['model',num2str(model)])); mkdir(fullfile(EXPT.modeldir,['model',num2str(model)])); end
+    
+    % generic design specification
+    def = spm_get_defaults;
+    job.timing.RT = EXPT.TR;
+    job.timing.units = 'secs';
+    job.timing.fmri_t = def.stats.fmri.t;
+    job.timing.fmri_t0 = def.stats.fmri.t0;
+    job.volt = 1;
+    job.fact = [];
+    job.cvi = def.stats.fmri.cvi;
+    job.global = 'None';
+    job.mthresh = 0.4;
+    
+    % hrf specification; use canonical hrf by default
+    if isfield(EXPT,'bases')
+        job.bases = EXPT.bases;
+    else
+        job.bases.hrf.derivs = [0 0];
+    end
+    
+    job0 = job;
+    
+    % subject-specific design specification
+    for subj = subjects
+        
+        job = job0;
+        
+        % overwrite existing files
+        modeldir = fullfile(EXPT.modeldir,['model',num2str(model)],['subj',num2str(subj)]);
+        if isdir(modeldir); rmdir(modeldir,'s'); end
+        mkdir(modeldir);
+        
+        job.dir{1} = modeldir;
+        S = EXPT.subject(subj);
+        job.mask{1} = fullfile(S.datadir,'mask.nii');
+        for i = 1:length(S.functional)
+            multi = EXPT.create_multi(model,subj,i);
+            save(fullfile(modeldir,['multi',num2str(i)]),'-struct','multi');
+            job.sess(i).hpf = def.stats.fmri.hpf;   % high-pass filter
+            job.sess(i).scans{1} = fullfile(S.datadir,['sru',S.functional{i}]);
+            job.sess(i).multi{1} = fullfile(modeldir,['multi',num2str(i)]);
+            job.sess(i).cond = struct('name',{},'onset',{},'duration',{},'tmod',{},'pmod',{},'orth',{});
+            job.sess(i).regress = [];
+            job.sess(i).multi_reg{1} = spm_file(fullfile(S.datadir,S.functional{i}),'prefix','rp_','ext','txt');    % motion regressors from realignment
+        end
+        
+        cd(modeldir);
+        
+        job = spm_run_fmri_spec(job);
+
+        if ~fake
+            load(job.spmmat{1});
+            spm_spm(SPM);
+        end
+
+        cd(cdir);
+    end
